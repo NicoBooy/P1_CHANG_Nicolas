0778332d51f59731dc276ae49bfdf217faaa29ec
NicoBooy <cnicolasc05@gmail.com>
2024-10-07 22:46:19 +0200

    Change in main

--- P1_CHANG_Nicolas.py
+++ P1_CHANG_Nicolas.py
@@ -89,8 +89,8 @@ def plot_feasible_region(x_opt, y_opt):
 # Main function to handle input arguments and execute the optimization
 def main():
     parser = argparse.ArgumentParser(description='Maximize the profit function subject to constraints.')
-    parser.add_argument('--xmax', type=float, default=15, help='Upper bound for x (default: 15)')
-    parser.add_argument('--ymax', type=float, default=15, help='Upper bound for y (default: 15)')
+    parser.add_argument('--xmax', type=float, default=15, help='Upper bound for x')
+    parser.add_argument('--ymax', type=float, default=15, help='Upper bound for y')
     
     args = parser.parse_args()
 
